ba4d63ae48cb3bd17bb76eda97da86d2b40bbf42
Emily Chew <emilychewsh@gmail.com>
2024-03-10 13:27:39 +1100

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Item({ name, category }) {
+
+  const [isAdd, setCart] = useState(false)
+
+  function handleCart () {
+    setCart(isAdd => !isAdd)
+  }
+
   return (
-    <li className="">
+    <li className={isAdd ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>
+        {isAdd ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
